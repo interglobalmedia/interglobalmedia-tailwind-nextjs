c3aaf57ba8ec7fdf8f1af486cb19b40d8f69cb52
Maria Campbell <interglobalmedia@gmail.com>
2022-06-16 19:34:13 -0400

    ITNJ-15 #resolve #comment add reading-time

--- layouts/PostLayout.js
+++ layouts/PostLayout.js
@@ -38,7 +38,8 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
                   <dt className="sr-only">Published on</dt>
                   <dd className="text-base font-medium leading-6 text-gray-500 dark:text-gray-400">
                     <time dateTime={date}>
-                      {new Date(date).toLocaleDateString(siteMetadata.locale, postDateTemplate)}
+                      {new Date(date).toLocaleDateString(siteMetadata.locale, postDateTemplate)} |{' '}
+                      {frontMatter.readingTime.text}
                     </time>
                   </dd>
                 </div>
