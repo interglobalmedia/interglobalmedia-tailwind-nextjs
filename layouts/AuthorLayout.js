3cc9b788074c196388f79e8cf630e6c399128796
Maria Campbell <interglobalmedia@gmail.com>
2022-06-16 13:20:55 -0400

    ITNJ-13 #resolve #comment import About scss module

--- layouts/AuthorLayout.js
+++ layouts/AuthorLayout.js
@@ -1,6 +1,7 @@
 import SocialIcon from '@/components/social-icons'
 import Image from '@/components/Image'
 import { PageSEO } from '@/components/SEO'
+import '../styles/partials/About.module.scss'
 
 export default function AuthorLayout({ children, frontMatter }) {
   const { name, avatar, occupation, company, email, twitter, linkedin, github } = frontMatter
