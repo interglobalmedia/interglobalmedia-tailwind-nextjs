4e0cb306f10ccec2aba39b9669087bad7ac8621a
Maria Campbell <interglobalmedia@gmail.com>
2022-06-19 10:49:44 -0400

    ITNJ-19 #resolve #comment add custom class

--- pages/projects.js
+++ pages/projects.js
@@ -19,8 +19,8 @@ export default function Projects({ initialDisplayPosts = [], pagination }) {
   return (
     <>
       <PageSEO title={`Projects - ${siteMetadata.author}`} description={siteMetadata.description} />
-      <div className="divide-y divide-gray-200 dark:divide-gray-700">
-        <div className="space-y-2 pt-6 pb-8 md:space-y-5">
+      <div className="divide-y divide-gray-200 dark:divide-gray-900">
+        <div className="projects-page-title-wrapper space-y-2 pt-6 pb-8 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             Search All Projects
           </h1>
