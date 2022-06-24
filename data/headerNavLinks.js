d0d2ffc04d99f4e99c1f1bf58171f64b735b99c4
Maria Campbell <interglobalmedia@gmail.com>
2022-06-24 17:29:47 -0400

    ITNJ-26 #resolve #comment add categories

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,6 +1,6 @@
 const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Categories' },
+  { href: '/categories', title: 'Categories' },
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
   { href: '/about', title: 'About' },
