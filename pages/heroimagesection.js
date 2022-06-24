b6018d8aa8fb87e3050a420a800d658fc5812af1
Maria Campbell <interglobalmedia@gmail.com>
2022-06-24 11:09:07 -0400

    ITNJ-23 #resolve #comment add new services and tips slider components

---
+++ pages/heroimagesection.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ServicesSlider from '../components/ServicesSlider'
+
+function HeroImageSection() {
+  return (
+    <div>
+      <ServicesSlider />
+    </div>
+  )
+}
+
+export default HeroImageSection
