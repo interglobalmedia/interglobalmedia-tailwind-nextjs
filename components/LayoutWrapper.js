0daecfde825264702947496e1ff8943e0872fb21
Maria Campbell <interglobalmedia@gmail.com>
2022-06-29 07:55:50 -0400

    ITNJ-38 #resolve #comment import Nav remove desktop nav TS related markup

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -1,50 +1,16 @@
-import { useRouter } from 'next/router'
-import siteMetadata from '@/data/siteMetadata'
-import headerNavLinks from '@/data/headerNavLinks'
 import Link from './Link'
-import SectionContainer from './SectionContainer'
 import Footer from './Footer'
-import MobileNav from './MobileNav'
-import ThemeSwitch from './ThemeSwitch'
+import Navigation from './Navigation'
+
 import '../styles/partials/LayoutWrapper.module.scss'
 
 const LayoutWrapper = ({ children }) => {
-  const router = useRouter()
   return (
-    <SectionContainer>
-      <div className="flex h-screen flex-col justify-between">
-        <header className="flex items-center justify-between py-10">
-          <div>
-            <Link href="/" aria-label={siteMetadata.headerTitle}>
-              <div className="flex items-center justify-between">
-                <div className="h-6 text-2xl font-semibold sm:block">
-                  <span className="globe sketch-highlight">Inter 🌐 Media</span>
-                </div>
-              </div>
-            </Link>
-          </div>
-          <div className="flex items-center text-base leading-5">
-            <div className="hidden sm:block">
-              {headerNavLinks.map((link) => (
-                <Link
-                  key={link.title}
-                  href={link.href}
-                  className={`${
-                    router.pathname === link.href ? 'active' : ''
-                  } menu-link p-1 font-medium text-gray-900 dark:text-gray-100 sm:p-4`}
-                >
-                  {link.title}
-                </Link>
-              ))}
-            </div>
-            <ThemeSwitch />
-            <MobileNav />
-          </div>
-        </header>
-        <main className="mb-auto">{children}</main>
-        <Footer />
-      </div>
-    </SectionContainer>
+    <>
+      <Navigation />
+      <main className="mb-auto">{children}</main>
+      <Footer />
+    </>
   )
 }
 
