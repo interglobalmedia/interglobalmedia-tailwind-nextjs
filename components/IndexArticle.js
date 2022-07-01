26f70b7b221bf9fa9a1f88ed717d7606224784f2
Maria Campbell <interglobalmedia@gmail.com>
2022-07-01 08:01:10 -0400

    ITNJ-44 #resolve #comment add proper image ratio change to intrinsic

--- components/IndexArticle.js
+++ components/IndexArticle.js
@@ -9,7 +9,7 @@ const IndexArticle = ({ slug, title, images, summary, isH2 = true }) => {
       <Link href={`/blog/${slug}`} title={title} className="w-full xl:w-auto">
         {/* eslint-disable-next-line @next/next/no-img-element */}
         {images.length > 0 && (
-          <Image alt={title} src={images} width={1280} height={720} layout="intrinsic" />
+          <Image alt={title} src={images} width={1280} height={960} layout="intrinsic" />
         )}
       </Link>
     </>
