e6555b23e7cf1804c7a3d9ad82bb5681d00d35a2
Maria Campbell <interglobalmedia@gmail.com>
2022-07-07 09:40:12 -0400

    ITNJ-65 #comment log GA page views

--- pages/_app.js
+++ pages/_app.js
@@ -13,6 +13,10 @@ import Analytics from '@/components/analytics'
 import LayoutWrapper from '@/components/LayoutWrapper'
 import { ClientReload } from '@/components/ClientReload'
 import { motion } from 'framer-motion'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+import * as gtag from '@/lib/gtag'
 
 const isDevelopment = process.env.NODE_ENV === 'development'
 const isSocket = process.env.SOCKET
@@ -24,6 +28,22 @@ const variants = {
 }
 
 export default function App({ Component, pageProps, router }) {
+  const pageRouter = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    pageRouter.events.on('routeChangeComplete', handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      pageRouter.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [pageRouter.events])
   return (
     <>
       <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
