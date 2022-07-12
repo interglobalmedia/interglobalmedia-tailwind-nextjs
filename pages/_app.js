62e2b7cd8002fd7af279658b3e2572a356d93004
Maria Campbell <interglobalmedia@gmail.com>
2022-07-12 06:56:19 -0400

    ITNJ-74 #resolve #comment add DisplayViews

--- pages/_app.js
+++ pages/_app.js
@@ -14,40 +14,45 @@ import LayoutWrapper from '@/components/LayoutWrapper'
 import { ClientReload } from '@/components/ClientReload'
 import { motion } from 'framer-motion'
 import PlausibleProvider from 'next-plausible'
+import DisplayViews from '@/components/DisplayViews'
 
 const isDevelopment = process.env.NODE_ENV === 'development'
 const isSocket = process.env.SOCKET
 
 const variants = {
-  hidden: { opacity: 0, x: -200, y: 0 },
-  enter: { opacity: 1, x: 0, y: 0 },
-  exit: { opacity: 0, x: 0, y: -100 },
+	hidden: { opacity: 0, x: -200, y: 0 },
+	enter: { opacity: 1, x: 0, y: 0 },
+	exit: { opacity: 0, x: 0, y: -100 },
 }
 
 export default function App({ Component, pageProps, router }) {
-  return (
-    <>
-      <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
-        <Head>
-          <meta content="width=device-width, initial-scale=1" name="viewport" />
-        </Head>
-        {isDevelopment && isSocket && <ClientReload />}
-        <Analytics />
-        <LayoutWrapper>
-          <motion.div
-            key={router.route}
-            initial="hidden"
-            animate="enter"
-            exit="exit"
-            variants={variants}
-            transition={{ duration: 0.5, type: 'tween' }}
-          >
-            <PlausibleProvider domain="interglobalmedianetwork.com">
-              <Component {...pageProps} />
-            </PlausibleProvider>
-          </motion.div>
-        </LayoutWrapper>
-      </ThemeProvider>
-    </>
-  )
+	return (
+		<>
+			<ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
+				<Head>
+					<meta
+						content="width=device-width, initial-scale=1"
+						name="viewport"
+					/>
+				</Head>
+				{isDevelopment && isSocket && <ClientReload />}
+				<Analytics />
+				<LayoutWrapper>
+					<motion.div
+						key={router.route}
+						initial="hidden"
+						animate="enter"
+						exit="exit"
+						variants={variants}
+						transition={{ duration: 0.5, type: 'tween' }}
+					>
+						<PlausibleProvider domain="interglobalmedianetwork.com">
+							<DisplayViews />
+							<Component {...pageProps} />
+						</PlausibleProvider>
+					</motion.div>
+				</LayoutWrapper>
+			</ThemeProvider>
+		</>
+	)
 }
