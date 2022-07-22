baa135c9cbc8b046e80bcf7f0112173ae7f37a5b
Maria Campbell <interglobalmedia@gmail.com>
2022-07-21 21:23:17 -0400

    ITNJ-83 #resolve #comment add twitterHandle domain

--- components/social-share/ShareTweet.js
+++ components/social-share/ShareTweet.js
@@ -1,7 +1,9 @@
 import SocialIcon from '@/components/social-icons'
+import siteMetadata from '@/data/siteMetadata'
 import '../../styles/partials/Share.module.scss'
 
 export default function ShareTweet({ url, title }) {
-	const intentUrl = `https://twitter.com/intent/tweet?url=${url}&text=${title} by @letsbsocial1`
+	const text = `I just read an article about`
+	const intentUrl = `https://twitter.com/intent/tweet?url=${url}&text=${text} "${title}" by ${siteMetadata.twitterHandle} on ${siteMetadata.domain}`
 	return <SocialIcon kind="twitter" href={intentUrl} size="6" />
 }
