eb414af6318747a40b6446cebdab6efef290b060
Maria Campbell <interglobalmedia@gmail.com>
2022-09-01 21:24:46 -0400

    ITNJ-93 #resolve #comment add skills related files

---
+++ components/Skills.js
@@ -0,0 +1,68 @@
+const Skills = () => {
+	return (
+		<section className="skills-content">
+			<ul className="abilities">
+				<li
+					className="ability-command-line"
+					title="command line interface"
+				>
+					CLI
+				</li>
+				<li className="ability-css3" title="css3">
+					CSS3
+				</li>
+				<li className="ability-ejs" title="ejs">
+					EJS
+				</li>
+				<li className="ability-express" title="express">
+					Express
+				</li>
+				<li className="ability-git" title="git">
+					{' '}
+					Git
+				</li>
+				<li className="ability-html5" title="html5">
+					HTML5
+				</li>
+				<li className="ability-jira" title="jira software">
+					Jira
+				</li>
+				<li className="ability-js" title="javascript">
+					JS
+				</li>
+				<li className="ability-markdown" title="markdown">
+					MD
+				</li>
+				<li className="ability-mongodb" title="mongodb">
+					MongoDB
+				</li>
+				<li className="ability-nextjs" title="nextjs">
+					NextJS
+				</li>
+				<li className="ability-nodejs" title="nodejs">
+					NodeJS
+				</li>
+				<li className="ability-npm" title="npm">
+					NPM
+				</li>
+				<li className="ability-react" title="react">
+					React
+				</li>
+				<li className="ability-sass" title="sass">
+					Sass
+				</li>
+				<li className="ability-sql" title="sql">
+					SQL
+				</li>
+				<li className="ability-vscode" title="vs code">
+					VS Code
+				</li>
+				<li className="ability-wordpress" title="wordpress">
+					WP
+				</li>
+			</ul>
+		</section>
+	)
+}
+
+export default Skills
