583e82e6b9878e2a930dae1ad027b11608429855
Maria Campbell <interglobalmedia@gmail.com>
2023-04-02 13:38:54 -0400

    ITNJ-115 #resolve #time 5m #comment remove ref to styled-components

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,4 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
 
 class MyDocument extends Document {
 	render() {
@@ -46,31 +45,6 @@ class MyDocument extends Document {
 				</body>
 			</Html>
 		)
-	}
-	static async getInitialProps(ctx) {
-		const sheet = new ServerStyleSheet()
-		const originalRenderPage = ctx.renderPage
-
-		try {
-			ctx.renderPage = () =>
-				originalRenderPage({
-					enhanceApp: (App) => (props) =>
-						sheet.collectStyles(<App {...props} />),
-				})
-
-			const initialProps = await Document.getInitialProps(ctx)
-			return {
-				...initialProps,
-				styles: (
-					<>
-						{initialProps.styles}
-						{sheet.getStyleElement()}
-					</>
-				),
-			}
-		} finally {
-			sheet.seal()
-		}
 	}
 }
 
